1bedc3f59d10671e2a424e95003068eb7ca38159
thinhh <thinh_trang942000@yahoo.com>
2022-03-04 20:17:16 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   return (
+<<<<<<< HEAD
     <>
       <h1>Hello</h1>
     </>
+=======
+    <h1>hello!dfdfdfsdfsfdsf</h1>
+>>>>>>> a11d319179576e22abc579f7114114d352236064
   );
 }
 
