64b23b1fb053e8329ef4994d094d2eb8da230f29
thinhh <35820288+thinhh@users.noreply.github.com>
2022-03-04 20:22:19 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
       <h1>Hello</h1>
     </>
-=======
-    <h1>hello!dfdfdfsdfsfdsf</h1>
->>>>>>> a11d319179576e22abc579f7114114d352236064
-  );
-}
+
 
 export default App;
